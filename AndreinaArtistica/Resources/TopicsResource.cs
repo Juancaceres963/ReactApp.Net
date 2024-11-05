888de4b17fa2d6eb3f913bcf0fd898bc8dda5817
Juancaceres963 <dnijuancaceres@gmail.com>
2024-11-05 18:40:16 -0300

    Create TopicsResource

---
+++ AndreinaArtistica/Resources/TopicsResource.cs
@@ -0,0 +1,34 @@
+﻿using AndreinaArtistica.Controllers.Parameters;
+using AndreinaArtistica.Models;
+using AndreinaArtistica.Models.DB;
+using AndreinaArtistica.Resources.Abstract;
+using AndreinaArtistica.Resources.Mappers;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Dynamic.Core;
+
+namespace AndreinaArtistica.Resources
+{
+    public class TopicesResource : ITopicsResource
+    {
+        private readonly AndreinartisticaContext _context;
+
+        public TopicesResource(AndreinartisticaContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<TopicViewModel>> GetTopics()
+        {
+            var topicsList = await GetTopicsFromDB();
+
+            var viewModel = topicsList.Select(topic => topic.MapToViewModel(topicsList);
+
+            return viewModel;
+        }
+        // TODO: Quizás mover estos métodos a una clase de base de datos para reutilizarlos
+        private async Task<List<Topic>> GetTopicsFromDB()
+        {
+            var topicsContext = _context.Topics;
+            return await topicsContext.ToListAsync();
+        }
+    }
+}

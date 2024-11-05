c376deac120f9243e17876fd2b7e6b86e1424062
Juancaceres963 <dnijuancaceres@gmail.com>
2024-11-04 21:46:38 -0300

    Create TopicMapper

---
+++ AndreinaArtistica/Resources/Mappers/TopicMapper.cs
@@ -0,0 +1,19 @@
+﻿using AndreinaArtistica.Models;
+using AndreinaArtistica.Models.DB;
+
+namespace AndreinaArtistica.Resources.Mappers
+{
+    public static class TopicMapper
+    {
+        public static TopicViewModel MapToViewModel(this Topic topic)
+        {
+            var viewModel = new TopicViewModel
+            {
+                Id = topic.Id,
+                Name = topic.Name
+            };
+
+            return viewModel;
+        }
+    } 
+}

634d7228b17892cb61aa6bbf9796f723621aa7cd
Juancaceres963 <dnijuancaceres@gmail.com>
2024-11-05 18:35:54 -0300

    Create ITopicsResource

---
+++ AndreinaArtistica/Resources/Abstract/ITopicsResource.cs
@@ -0,0 +1,10 @@
+﻿using AndreinaArtistica.Controllers.Parameters;
+using AndreinaArtistica.Models;
+
+namespace AndreinaArtistica.Resources.Abstract
+{
+    public interface ITopicsResource
+    {
+        public Task<IEnumerable<TopicViewModel>> GetTopics();
+    }
+}

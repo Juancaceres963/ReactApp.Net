0549a4128e6f8fd0f90b2154c19eb811ed8b8a35
Juancaceres963 <dnijuancaceres@gmail.com>
2024-11-05 18:04:36 -0300

    Create TopicsController

---
+++ AndreinaArtistica/Controllers/TopicsController.cs
@@ -0,0 +1,26 @@
+﻿using AndreinaArtistica.Controllers.Parameters;
+using AndreinaArtistica.Models;
+using AndreinaArtistica.Resources.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AndreinaArtistica.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TopicsController : ControllerBase
+    {
+        private readonly ITopicsResource _TopicsResource;
+
+        public TopicsController(ITopicsResource topicsResource)
+        {
+            _TopicsResource = topicsResource;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TopicViewModel>>> Get([FromQuery])
+        {
+            var result = await _TopicsResource.GetArtPieces();
+            return Ok(result);
+        }
+    }
+}

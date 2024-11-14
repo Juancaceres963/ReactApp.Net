80867134097f378ef148c44ebd98d92c69adb599
Juancaceres963 <dnijuancaceres@gmail.com>
2024-11-14 19:30:14 -0300

    Create CategoriesController.

---
+++ AndreinaArtistica/Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using AndreinaArtistica.Models;
+using AndreinaArtistica.Resources.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AndreinaArtistica.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoriesResource _categoriesResource
+
+        public CategoriesController (ICategoriesResource CategoriesResource)
+        {
+            _categoriesResource = CategoriesResource;
+        }
+        [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<CategoryViewModel>>> Get()
+        {
+            var result = await _categoriesResource.GetCategories();
+            return Ok(result);
+        }
+    }
+}

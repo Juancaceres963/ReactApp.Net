32e32b7771221badf86ab0fef0e7fbe2684d5c9f
Juancaceres963 <dnijuancaceres@gmail.com>
2024-11-14 19:40:55 -0300

    Create CategoryMapper.

---
+++ AndreinaArtistica/Resources/Mappers/CategoryMapper.cs
@@ -0,0 +1,18 @@
+﻿using AndreinaArtistica.Models;
+using AndreinaArtistica.Models.DB;
+
+namespace AndreinaArtistica.Resources.Mappers
+{
+    public static class CategoryMapper
+    {
+        public static CategoryViewModel MapToViewModel(this Category category, List<Category> Category)
+        {
+            var viewModel = new CategoryViewModel
+            {
+                Id = category.Id,
+                Name = category.Name
+            };
+            return viewModel;
+        }
+    }
+}

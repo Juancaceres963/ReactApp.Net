e6c9587eaa8bfae3ec892336e7a34a81bae48ff2
Juancaceres963 <dnijuancaceres@gmail.com>
2024-11-14 18:09:10 -0300

    Create TechniquesResource.

---
+++ AndreinaArtistica/Resources/TechniquesResource.cs
@@ -0,0 +1,39 @@
+﻿using AndreinaArtistica.Helpers.Abstract;
+using AndreinaArtistica.Models;
+using AndreinaArtistica.Models.DB;
+using AndreinaArtistica.Resources.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Dynamic.Core;
+
+namespace AndreinaArtistica.Resources
+{
+    public class TechniqueResource : ITechniquesResource
+    {
+        private readonly AndreinartisticaContext _context;
+        private readonly IDatabaseHelper _databaseHelper;
+
+        public TechniqueResource(AndreinartisticaContext context, IDatabaseHelper databaseHelper)
+        {
+            _context = context;
+            _databaseHelper = databaseHelper;
+        }
+        public async Task<IEnumerable<TechniqueViewModel>> GetTechniques()
+        {
+            var techniqueList = await _databaseHelper.GetTechniquesFromDB();
+
+            var viewModel = techniqueList.Select(technique => new TechniqueViewModel
+            {
+                Id = technique.Id,
+                Name = technique.Name
+            });
+
+            return viewModel;
+        }
+        private async Task<List<Technique>> GetTechniquesFromDB()
+        {
+            var techniquesContext = _context.Techniques
+                .Select(technique => new Technique { Id = technique.Id, Name = technique.Name });
+            return await techniquesContext.ToListAsync();
+        }
+    }
+}

e6da150016673af71f1016f36ebd8d7397533e02
Juancaceres963 <dnijuancaceres@gmail.com>
2024-11-14 18:11:24 -0300

    Create TechniqueController.

---
+++ AndreinaArtistica/Controllers/TechniqueController.cs
@@ -0,0 +1,26 @@
+﻿using AndreinaArtistica.Models;
+using AndreinaArtistica.Resources.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AndreinaArtistica.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TechniquesController : ControllerBase
+    {
+        private readonly ITechniquesResource _techniquesResource;
+
+        public TechniquesController(ITechniquesResource techniqueResource)
+        {
+            _techniquesResource = techniqueResource;
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<TechniqueViewModel>>> Get()
+        {
+            var result = await _techniquesResource.GetTechniques();
+            return Ok(result);
+        }
+    }
+}

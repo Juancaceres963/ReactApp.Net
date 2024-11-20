ed9bd4ccc8cbf0045abb2d5dc8c90010a7400f62
Juancaceres963 <dnijuancaceres@gmail.com>
2024-11-20 12:33:09 -0300

    Create MaxPriceMapper.

---
+++ AndreinaArtistica/Resources/Mappers/MaxPriceMapper.cs
@@ -0,0 +1,18 @@
+﻿using AndreinaArtistica.Models;
+using AndreinaArtistica.Models.DB;
+
+namespace AndreinaArtistica.Resources.Mappers
+{
+    public static class MaxPriceMapper
+    {
+        public static MaxPriceViewModel MapToViewModel(this ArtPiece artPiece, List<MaxPrice> MaxPrice)
+        {
+            var viewModel = new MaxPriceViewModel
+            {
+                Price = artPiece.Price
+            };
+
+            return viewModel;
+        }
+    }
+}

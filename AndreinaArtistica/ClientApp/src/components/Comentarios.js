204810ec3ff0cdcfb65eaec21b377c55e07ab3c9
Juancaceres963 <dnijuancaceres@gmail.com>
2024-11-30 04:31:20 -0300

    Main components created.

---
+++ AndreinaArtistica/ClientApp/src/components/Comentarios.js
@@ -0,0 +1,10 @@
+﻿function Comentarios() {
+
+    return (
+        <div>
+            <h1>Contacto</h1>
+        </div>
+    );
+}
+
+export default Comentarios;
---
+++ AndreinaArtistica/ClientApp/src/components/Galeria.js
@@ -0,0 +1,10 @@
+﻿function Galeria() {
+
+    return (
+        <div>
+            <h1>Contacto</h1>
+        </div>
+    );
+}
+
+export default Galeria;

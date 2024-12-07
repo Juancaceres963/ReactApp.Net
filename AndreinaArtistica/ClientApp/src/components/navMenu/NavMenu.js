6fed3b8c01e024f9164a3b9d19bf0b3d18dee3bf
Juancaceres963 <dnijuancaceres@gmail.com>
2024-12-07 20:49:42 -0300

    Changes class component to funtion component.

--- AndreinaArtistica/ClientApp/src/components/navMenu/NavMenu.js
+++ AndreinaArtistica/ClientApp/src/components/navMenu/NavMenu.js
@@ -1,52 +1,68 @@
-import React, { Component } from 'react';
-import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
-import { Link } from 'react-router-dom';
-import './NavMenu.css';
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import "./NavMenu.css";
 
-export class NavMenu extends Component {
-  static displayName = NavMenu.name;
+const NavMenu = () => {
+  const [collapsed, setCollapsed] = useState(true);
 
-  constructor (props) {
-    super(props);
+  const toggleNavbar = () => {
+    setCollapsed(!collapsed);
+  };
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
+  return (
+    <header>
+      <Navbar
+        className="navbar navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3"
+        container
+        light
+      >
+        <NavbarBrand href="/">
+          <img
+            className="nav-logo-img"
+            alt="logo"
+            src="https://i.imgur.com/0nCLxdQ.png"
+          />
+        </NavbarBrand>
+        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+        <Collapse
+          className="d-sm-inline-flex flex-sm-row-reverse"
+          isOpen={!collapsed}
+          navbar
+        >
+          <ul className="navbar-nav navbar-list flex-grow">
+            <NavItem>
+              <NavLink tag={Link} className="text-white" to="/">
+                SOBRE MI
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={Link} className="text-white" to="/galeria">
+                GALERIA
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={Link} className="text-white" to="/comentarios">
+                COMENTARIOS
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={Link} className="text-white" to="/contacto">
+                CONTACTO
+              </NavLink>
+            </NavItem>
+          </ul>
+        </Collapse>
+      </Navbar>
+    </header>
+  );
+};
 
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
-
-  render() {
-    return (
-      <header>
-        <Navbar className="navbar navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-            <NavbarBrand href="/">
-                <img className="nav-logo-img" alt="logo" src="https://i.imgur.com/0nCLxdQ.png"/>
-            </NavbarBrand>
-          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-          <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-                    <ul className="navbar-nav navbar-list flex-grow">
-              <NavItem>
-                <NavLink tag={Link} className="text-white" to="/">SOBRE MI</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-white" to="/galeria">GALERIA</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-white" to="/comentarios">COMENTARIOS</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-white" to="/contacto">CONTACTO</NavLink>
-              </NavItem>
-            </ul>
-          </Collapse>
-        </Navbar>
-      </header>
-    );
-  }
-}
+export default NavMenu;

69809502e74cf6c5170f78e4c49fb36a9432d195
Juancaceres963 <dnijuancaceres@gmail.com>
2024-12-09 15:00:26 -0300

    Edit Home.js

--- AndreinaArtistica/ClientApp/src/pages/Home/Home.js
+++ AndreinaArtistica/ClientApp/src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css";
+import Button from "../../components/Button/Button";
 
 function Home() {
   return (
@@ -17,24 +18,31 @@ function Home() {
               src="https://i.imgur.com/STSCdXA.png"
               alt="Logo y firma art�stica de la artista Andreina O."
             />
-            <h2 className="text-below-rigth type-AB">GALERIA VIRTUAL</h2>
-            {/* <img
-              className="image-below-rigth"
-              src="https://i.imgur.com/S99eXq8.png"
-              alt="Logo y firma art�stica de la artista Andreina O."
-            /> */}
+            <h2 className="text-below-rigth archivo-black-regular">GALERIA VIRTUAL</h2>
           </div>
         </section>
-        <section>
-          <div>
+        <section className="container presentacion">
+          <div className="image-side">
             <img
               className="image-perfil"
               src="https://i.imgur.com/zfp9IQ5.png"
               alt="Foto de perfeil Andreina O."
             />
           </div>
-          <div>
-
+          <div className="text-side">
+            <h1 className="italianno-regular">Andreina Orellana</h1>
+            <p>
+              A través del uso magistral de colores puros e intensos, esta
+              talentosa artista venezolana no solo pinta lienzos, sino que
+              además teje historias visuales que capturan la esencia misma de
+              los sentimientos humanos. Cada trazo y matiz busca resaltar en la
+              belleza estética, las virtudes más profundas de la experiencia
+              humana.
+            </p>
+            <div className="promotion">
+              <p>Retratos, composiciones personalizadas por ENCARGO... </p>
+              <Button texto="Encarga tu obra" ruta="./galeria" />
+            </div>
           </div>
         </section>
       </main>

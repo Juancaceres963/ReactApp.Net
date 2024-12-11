7f8fcd53c2bbdd85b98433072220538327cf5d64
Juancaceres963 <dnijuancaceres@gmail.com>
2024-12-11 05:49:23 -0300

    Create gallery-home section.

--- AndreinaArtistica/ClientApp/src/pages/Home/Home.js
+++ AndreinaArtistica/ClientApp/src/pages/Home/Home.js
@@ -18,7 +18,9 @@ function Home() {
               src="https://i.imgur.com/STSCdXA.png"
               alt="Logo y firma art�stica de la artista Andreina O."
             />
-            <h2 className="text-below-rigth archivo-black-regular">GALERIA VIRTUAL</h2>
+            <h2 className="text-below-rigth archivo-black-regular">
+              GALERIA VIRTUAL
+            </h2>
           </div>
         </section>
         <section className="container presentacion">
@@ -44,6 +46,33 @@ function Home() {
               <Button texto="Encarga tu obra" ruta="./galeria" />
             </div>
           </div>
+        </section>
+        <section class="gallery-home">
+          <h2 class="italianno-regular gallery-home-title">Galería</h2>
+          <div class="gallery-container">
+            <div class="gallery-item" id="item-1">
+              <img src="https://i.imgur.com/TstCfOp.png" alt="Categoría Retratos" />
+            </div>
+            <div class="gallery-item" id="item-2">
+              <img src="https://i.imgur.com/Xby40x9.png" alt="Categoría Arte Religioso" />
+            </div>
+            <div class="gallery-item" id="item-3">
+              <img src="https://i.imgur.com/U5DlRB4.png" alt="Categoría Retratos de Mascotas" />
+            </div>
+            <div class="gallery-item" id="item-4">
+              <img src="https://i.imgur.com/u5eaXDR.png" alt="Categoría Rostros y figuras humanas" />
+            </div>
+            <div class="gallery-item" id="item-5">
+              <img src="https://i.imgur.com/riRDxEE.png" alt="Categoría Animales" />
+            </div>
+            <div class="gallery-item" id="item-6">
+              <img src="https://i.imgur.com/o10eSsj.png" alt="Categoría Paisajes" />
+            </div>
+            <div class="gallery-item" id="item-7">
+              <img src="https://i.imgur.com/9FXbQIk.png" alt="Categoría Naturaleza" />
+            </div>
+          </div>
+          <Button texto="Ir a galeria" ruta="./galeria" />
         </section>
       </main>
     </div>

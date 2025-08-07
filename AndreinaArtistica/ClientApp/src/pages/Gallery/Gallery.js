91d5dc68db1b5e7fc2650c9846f35c46e92f5cfc
Juan Cáceres <dnijuancaceres@gmail.com>
2025-08-07 16:51:10 -0300

    Editing pages and footer styles  component

--- AndreinaArtistica/ClientApp/src/pages/Gallery/Gallery.js
+++ AndreinaArtistica/ClientApp/src/pages/Gallery/Gallery.js
@@ -1,10 +1,22 @@
-﻿ function Gallery() {
+﻿import "./Gallery.css";
+import Footer from "../../components/Footer/Footer";
+import Title from "../../components/Title/Title";
+import GalleryFilter from "../../components/GalletyFilter/GalleryFilter";
+import GalleryGrid from "../../components/GalleryGrid/GalleryGrid";
 
-    return (
-        <div>
-            <h1>Gallery</h1>
+const Gallery = () => {
+  return (
+    <div>
+      <div className="gallery-section">
+        <GalleryFilter />
+        <div className="gallery-container">
+          <Title text="Galeria" />
+          <GalleryGrid />
         </div>
-    );
-}
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 export default Gallery;
--- AndreinaArtistica/ClientApp/src/pages/Home/Home.js
+++ AndreinaArtistica/ClientApp/src/pages/Home/Home.js
@@ -1,4 +1,3 @@
-import React, { useEffect } from "react";
 import "./Home.css";
 import Banner from "../../components/Banner/Banner";
 import AboutMe from "../../components/AboutMe/AboutMe";
@@ -6,42 +5,6 @@ import Carousel from "../../components/Carousel/carousel"
 import Footer from '../../components/Footer/Footer';
 
 function Home() {
-  useEffect(() => {
-    const carousel = document.getElementById("carousel");
-    const items = document.querySelectorAll(".carousel-item");
-    const totalItems = items.length;
-    const angle = 360 / totalItems;
-    let currentAngle = 0;
-
-    // Posicionar cada elemento en un círculo
-    items.forEach((item, index) => {
-      item.style.transform = `rotateY(${index * angle}deg) translateZ(300px)`;
-    });
-
-    // Funciones para girar el carrusel
-    const nextBtn = document.getElementById("nextBtn");
-    const prevBtn = document.getElementById("prevBtn");
-
-    if (nextBtn && prevBtn && carousel) {
-      nextBtn.addEventListener("click", () => {
-        currentAngle -= angle;
-        carousel.style.transform = `rotateY(${currentAngle}deg)`;
-      });
-
-      prevBtn.addEventListener("click", () => {
-        currentAngle += angle;
-        carousel.style.transform = `rotateY(${currentAngle}deg)`;
-      });
-    }
-
-    // Limpieza para evitar fugas de memoria
-    return () => {
-      if (nextBtn && prevBtn) {
-        nextBtn.removeEventListener("click", () => {});
-        prevBtn.removeEventListener("click", () => {});
-      }
-    };
-  }, []); // Se ejecuta una sola vez al montar el componente
 
   return (
     <div>
